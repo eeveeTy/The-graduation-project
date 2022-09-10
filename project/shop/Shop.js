87911caf3a1d7ab0afc19ef49052e199a7d40072
Dmitry Tsivatkou <eeveety@gmail.com>
2022-09-10 19:26:42 +0300

    add js background

--- project/shop/Shop.js
+++ project/shop/Shop.js
@@ -1,3 +1,51 @@
-var button = document.getElementById('input_button_bg_change');
-var body = document.getElementsByTagName('body')[0];
-var img = document.createElement('img');
+function contentTShirtsDisplay() {
+    var SearchContent = document.getElementsByClassName('content__t-shirts');
+    for (var i = 0; 1 < SearchContent.length; i++) {
+        SearchContent[i].style.display = 'none';
+    }
+}
+
+function contentPantsAndShortsDisplay() {
+    var SearchContent = document.getElementsByClassName('content__pants-and-shorts');
+    for (var i = 0; 1 < SearchContent.length; i++) {
+        SearchContent[i].style.display = 'none';
+    }
+}
+
+function contentHoodieDisplay() {
+    var SearchContent = document.getElementsByClassName('content__hoodie');
+    for (var i = 0; 1 < SearchContent.length; i++) {
+        SearchContent[i].style.display = 'none';
+    }
+}
+
+function contentHatsDisplay() {
+    var SearchContent = document.getElementsByClassName('content__hats');
+    for (var i = 0; 1 < SearchContent.length; i++) {
+        SearchContent[i].style.display = 'none';
+    }
+}
+
+function contentMusicDisplay() {
+    var SearchContent = document.getElementsByClassName('content__music');
+    for (var i = 0; 1 < SearchContent.length; i++) {
+        SearchContent[i].style.display = 'none';
+    }
+}
+
+var background = 0;
+
+function backgroundChange() {
+    var SearchContentBody = document.getElementById('background-img');
+    var SearchContentButtonBackground = document.getElementById('button-hot-pink');
+    if (background !== 0) {
+        SearchContentBody.style.backgroundImage = 'url(img/site-background-image-planet-her.jpg)';
+        SearchContentButtonBackground.style.backgroundImage = 'url(img/site-background-image-hot-pink.jpg)';
+        ++background;
+    } else {
+        SearchContentBody.style.backgroundImage = 'url(img/site-background-image-hot-pink.jpg)';
+        SearchContentButtonBackground.style.backgroundImage = 'url(img/site-background-image-planet-her.jpg)';
+        --background;
+    }
+    console.log('bac', background);
+}
