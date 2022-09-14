f29070a19adc6ead2faf6dbe86896cea0233f85c
Dmitry Tsivatkou <eeveety@gmail.com>
2022-09-14 13:07:01 +0300

    fixet img

--- project/shop/Shop.js
+++ project/shop/Shop.js
@@ -1,51 +1,54 @@
+
 function contentTShirtsDisplay() {
-    var SearchContent = document.getElementsByClassName('content__t-shirts');
+    var SearchContent = document.getElementsByClassName('grid-uniform__content--t-shirts');
     for (var i = 0; 1 < SearchContent.length; i++) {
         SearchContent[i].style.display = 'none';
     }
 }
 
+
 function contentPantsAndShortsDisplay() {
-    var SearchContent = document.getElementsByClassName('content__pants-and-shorts');
+    var SearchContent = document.getElementsByClassName('grid-uniform__content--pantsAndShorts');
     for (var i = 0; 1 < SearchContent.length; i++) {
         SearchContent[i].style.display = 'none';
     }
 }
 
 function contentHoodieDisplay() {
-    var SearchContent = document.getElementsByClassName('content__hoodie');
+    var SearchContent = document.getElementsByClassName('grid-uniform__content--hoodie');
     for (var i = 0; 1 < SearchContent.length; i++) {
         SearchContent[i].style.display = 'none';
     }
 }
 
 function contentHatsDisplay() {
-    var SearchContent = document.getElementsByClassName('content__hats');
+    var SearchContent = document.getElementsByClassName('grid-uniform__content--hats');
     for (var i = 0; 1 < SearchContent.length; i++) {
         SearchContent[i].style.display = 'none';
     }
 }
 
 function contentMusicDisplay() {
-    var SearchContent = document.getElementsByClassName('content__music');
+    var SearchContent = document.getElementsByClassName('grid-uniform__content--music');
     for (var i = 0; 1 < SearchContent.length; i++) {
         SearchContent[i].style.display = 'none';
     }
 }
 
-var background = 0;
+var backgroundImg = 0;
 
 function backgroundChange() {
     var SearchContentBody = document.getElementById('background-img');
     var SearchContentButtonBackground = document.getElementById('button-hot-pink');
-    if (background !== 0) {
+    if (backgroundImg !== 0) {
         SearchContentBody.style.backgroundImage = 'url(img/site-background-image-planet-her.jpg)';
         SearchContentButtonBackground.style.backgroundImage = 'url(img/site-background-image-hot-pink.jpg)';
-        ++background;
+        ++backgroundImg;
     } else {
         SearchContentBody.style.backgroundImage = 'url(img/site-background-image-hot-pink.jpg)';
         SearchContentButtonBackground.style.backgroundImage = 'url(img/site-background-image-planet-her.jpg)';
-        --background;
+        --backgroundImg;
     }
-    console.log('bac', background);
+    console.log('bac', backgroundImg);
 }
+
