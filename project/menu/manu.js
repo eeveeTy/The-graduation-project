825a52285640878ab54bdc7e202f22001563635b
Dmitry Tsivatkou <eeveety@gmail.com>
2022-09-14 15:20:38 +0300

    add menu

---
+++ project/menu/manu.js
@@ -0,0 +1,6 @@
+function imgTest() {
+    var SearchContent = document.getElementsByClassName('img-position');
+    for (var i = 0; 1 < SearchContent.length; i++) {
+        SearchContent[i].style.backgroundColor = 'yellow';
+    }
+}
