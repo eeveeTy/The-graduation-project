14ecd820dba999a4fea9c64fac7c9dcf142bed40
Dmitry Tsivatkou <eeveety@gmail.com>
2022-09-25 16:13:38 +0300

    first try

--- project/shop/Shop.js
+++ project/shop/Shop.js
@@ -1,54 +1,52 @@
 
 function contentTShirtsDisplay() {
-    var SearchContent = document.getElementsByClassName('grid-uniform__content--t-shirts');
-    for (var i = 0; 1 < SearchContent.length; i++) {
-        SearchContent[i].style.display = 'none';
+    var searchContent = document.getElementsByClassName('grid-uniform__content--t-shirts');
+    for (var i = 0; 1 < searchContent.length; i++) {
+        searchContent[i].style.display = 'none';
     }
 }
 
 
 function contentPantsAndShortsDisplay() {
-    var SearchContent = document.getElementsByClassName('grid-uniform__content--pantsAndShorts');
-    for (var i = 0; 1 < SearchContent.length; i++) {
-        SearchContent[i].style.display = 'none';
+    var searchContent = document.getElementsByClassName('grid-uniform__content--pantsAndShorts');
+    for (var i = 0; 1 < searchContent.length; i++) {
+        searchContent[i].style.display = 'none';
     }
 }
 
 function contentHoodieDisplay() {
-    var SearchContent = document.getElementsByClassName('grid-uniform__content--hoodie');
-    for (var i = 0; 1 < SearchContent.length; i++) {
-        SearchContent[i].style.display = 'none';
+    var searchContent = document.getElementsByClassName('grid-uniform__content--hoodie');
+    for (var i = 0; 1 < searchContent.length; i++) {
+        searchContent[i].style.display = 'none';
     }
 }
 
 function contentHatsDisplay() {
-    var SearchContent = document.getElementsByClassName('grid-uniform__content--hats');
-    for (var i = 0; 1 < SearchContent.length; i++) {
-        SearchContent[i].style.display = 'none';
+    var searchContent = document.getElementsByClassName('grid-uniform__content--hats');
+    for (var i = 0; 1 < searchContent.length; i++) {
+        searchContent[i].style.display = 'none';
     }
 }
 
 function contentMusicDisplay() {
-    var SearchContent = document.getElementsByClassName('grid-uniform__content--music');
-    for (var i = 0; 1 < SearchContent.length; i++) {
-        SearchContent[i].style.display = 'none';
+    var searchContent = document.getElementsByClassName('grid-uniform__content--music');
+    for (var i = 0; 1 < searchContent.length; i++) {
+        searchContent[i].style.display = 'none';
     }
 }
 
 var backgroundImg = 0;
 
 function backgroundChange() {
-    var SearchContentBody = document.getElementById('background-img');
-    var SearchContentButtonBackground = document.getElementById('button-hot-pink');
+    var searchContentBody = document.getElementById('background-img');
+    var searchContentButtonBackground = document.getElementById('button-hot-pink');
     if (backgroundImg !== 0) {
-        SearchContentBody.style.backgroundImage = 'url(img/site-background-image-planet-her.jpg)';
-        SearchContentButtonBackground.style.backgroundImage = 'url(img/site-background-image-hot-pink.jpg)';
+        searchContentBody.style.backgroundImage = 'url(img/site-background-image-planet-her.jpg)';
+        searchContentButtonBackground.style.backgroundImage = 'url(img/site-background-image-hot-pink.jpg)';
         ++backgroundImg;
     } else {
-        SearchContentBody.style.backgroundImage = 'url(img/site-background-image-hot-pink.jpg)';
-        SearchContentButtonBackground.style.backgroundImage = 'url(img/site-background-image-planet-her.jpg)';
+        searchContentBody.style.backgroundImage = 'url(img/site-background-image-hot-pink.jpg)';
+        searchContentButtonBackground.style.backgroundImage = 'url(img/site-background-image-planet-her.jpg)';
         --backgroundImg;
     }
-    console.log('bac', backgroundImg);
 }
-
